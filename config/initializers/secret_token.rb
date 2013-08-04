5a7e8945e829cb18eb9a7a39673a8809c315732e
Niket Patel <niket@niketpatel.com>
2013-08-04 12:09:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sywebn::Application.config.secret_token = 'd942dc4eaf61b513e3c8e05dda02cc20932c22298eac8e8592ae88819bd7efa8978c184862321a80dbb9b1374493d0edae45383db7787289643895d96a3c2763'
